abbf3ce71ed2586ffbd2b34d6fcd6bcba50ea778
Ahsaber00 <ahmedsaber.4200@gmail.com>
2023-09-05 23:03:30 +0300

    commit

---
+++ Assingment _1.php
@@ -0,0 +1,156 @@
+<?php
+
+/*
+for($i=1;$i<=10;$i++)
+{
+    if($i==10)
+    {
+        echo $i;
+    }
+    else
+    {
+        echo "$i-";
+    }
+}
+*/
+
+
+
+
+/*
+for($i=1;$i<=50;$i++)
+{
+    if($i%3==0)
+    {
+        echo "Fizz ";
+    }
+    else if($i%5==0)
+    {
+        echo "Buzz ";
+    }
+    else if($i%3==0 && $i%5==0)
+    {
+        echo "FizzBuzz ";
+    }
+    else
+    {
+        echo "$i ";
+    }
+}
+*/
+
+
+
+/*
+$sum=0;
+
+for($i=0;$i<=30;$i++)
+
+{
+    $sum+=$i;
+
+}
+echo "The sum between 0 and 30 is $sum";
+*/
+
+
+
+
+/*
+$number=4;
+$factorial=1;
+for($i=$number;$i>=1;$i--)
+{
+    $factorial*=$i;
+}
+echo $factorial;
+*/
+
+
+
+
+/*
+$i=1;
+while($i<=15)
+{
+    if($i%2!=0)
+    {
+        echo "$i\n";
+    }
+    $i++;
+    
+}
+*/
+
+
+
+
+/*
+$i=1;
+while($i<=15)
+{
+    if($i%2==0)
+    {
+        echo "$i\n";
+    }
+    $i++;
+    
+}
+*/
+
+
+
+
+/*
+$i=1;
+do
+{
+    if($i%3==0)
+    {
+        echo "$i\n";
+    }
+    $i++;
+
+}while($i<=100)
+*/
+
+
+
+
+/*
+$num1=3;
+$num2=2;
+$op='+';
+
+if (is_numeric($num1) && is_numeric($num2)) {
+            if ($op == "+") {
+                echo $num1 + $num2;
+            } elseif ($op == "-") {
+                echo $num1 - $num2;
+             } elseif ($op == "*") {
+           echo $num1 * $num2;
+          } elseif ($op == "/") {
+                if ($num2 == 0) {
+                    echo "Not Allowed";
+                 } else {
+                    echo $num1 / $num2;
+                 }
+            } elseif ($op == "**") {
+                echo $num1 ** $num2;
+             } elseif ($op == "%") {
+                if ($num2 == 0 || $num2 > $num1) {
+                    echo "Not Allowed";
+                } else {
+                    echo $num1 % $num2;
+                 }
+            } else {
+                 echo $num1 + $num2;
+             }
+        } else {
+            echo "Not Allowed Data Type";
+         }
+        
+ */   
+         
+
+?>
